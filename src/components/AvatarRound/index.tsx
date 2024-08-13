8f95e7097f5f606f6e627863c655e0c5e8a09154
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-13 18:22:51 -0300

    styling the avatar with styled-components

--- src/components/AvatarRound/index.tsx
+++ src/components/AvatarRound/index.tsx
@@ -1,5 +1,21 @@
+"use client";
+
+import styled from "styled-components";
+
+const StyledAvatar = styled.span`
+	//Background
+	background-image: url("/images/image-avatar.png");
+	background-size: cover;
+	background-position: center;
+	//Dimensioning
+	border-radius: 50%;
+
+	width: 24px;
+	height: 24px;
+`;
+
 const AvatarRound = () => {
-	return <div>AvatarRound</div>;
+	return <StyledAvatar />;
 };
 
 export default AvatarRound;
