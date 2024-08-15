bfb73cab95da389bafc00bb4b6fa76ea965bdf8a
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-15 17:29:43 -0300

    Refactoring the shopping cart icon component since I'll have the reutilize it in other component

--- src/components/icons/IconShoppingCart/index.tsx
+++ src/components/icons/IconShoppingCart/index.tsx
@@ -1,7 +1,30 @@
-import styled from "./IconShoppingCart.module.css";
+"use client";
 
-const IconShoppingCart = () => {
-	return <button className={styled.iconShoppingCart} />;
+import styled from "styled-components";
+
+interface IIconProps {
+	color?: string;
+}
+
+const StyledIconShoppingCart = styled.i<IIconProps>`
+	.iconShoppingCart {
+		/* Background */
+		background-image: url("./icon-cart.svg");
+
+		background-color: ${props => props.color || "transparent"};
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: contain;
+		/* Dimensioning */
+		height: 21.82px;
+		width: 20px;
+
+		display: block;
+	}
+`;
+
+const IconShoppingCart: React.FC<IIconProps> = ({ color }) => {
+	return <StyledIconShoppingCart color={color} />;
 };
 
 export default IconShoppingCart;
