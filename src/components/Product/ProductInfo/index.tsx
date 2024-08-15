a275f3207fb70079f228dc33ce555069952a86d4
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-15 02:50:00 -0300

    Finishing the main section with the product info

---
+++ src/components/Product/ProductInfo/index.tsx
@@ -0,0 +1,31 @@
+import ShoppingButtons from "@/components/ShoppingButtons";
+
+import styles from "./ProductInfo.module.scss";
+
+function ProductInfo() {
+	return (
+		<main>
+			<article className={styles.productArticle}>
+				<span className={styles["productArticle__brandName"]}>
+					Sneaker Company
+				</span>
+				<h1 className={styles["productArticle__title"]}>
+					Fall Limited Edition Sneakers
+				</h1>
+				<p className={styles["productArticle__description"]}>
+					These low-profile sneakers are your perfect casual wear companion.
+					Featuring a durable rubber outer sole, they'll withstand everything
+					the weather can offer.
+				</p>
+			</article>
+			<div className={styles["pricing"]}>
+				<span className={styles["pricing__finalPrice"]}>$125.00</span>
+				<span className={styles["pricing__discount"]}>50%</span>
+				<span className={styles["pricing__fullPrice"]}>$250.00</span>
+			</div>
+			<ShoppingButtons />
+		</main>
+	);
+}
+
+export default ProductInfo;
