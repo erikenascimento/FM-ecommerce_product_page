98ac0a3c5ed08b8ef74c6295231f13929241460f
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-16 23:19:05 -0300

    Creating icon components for the shopping buttons

---
+++ src/components/icons/IconShoppingMinus/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./IconShoppingMinus.module.scss";
+
+const IconShoppingMinus = () => {
+	return <span className={styles.iconShoppingMinus}></span>;
+};
+
+export default IconShoppingMinus;
---
+++ src/components/icons/IconShoppingPlus/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./IconShoppingPlus.module.scss";
+
+const IconShoppingPlus = () => {
+	return <span className={styles.iconShoppingPlus}></span>;
+};
+
+export default IconShoppingPlus;
