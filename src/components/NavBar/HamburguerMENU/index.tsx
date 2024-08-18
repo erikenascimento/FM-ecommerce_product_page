b2517b2e160ccc17c13041d4e8c9cda022de50e2
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-18 00:34:01 -0300

    Completely finished the drawer by overriding some styles from MUI

--- src/components/NavBar/HamburguerMENU/index.tsx
+++ src/components/NavBar/HamburguerMENU/index.tsx
@@ -9,6 +9,9 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
+// Still MUI but outside the default imports
+import CloseIcon from "@mui/icons-material/Close";
+import { IconButton } from "@mui/material";
 
 const HamburguerMENU = () => {
 	const [open, setOpen] = React.useState(false);
@@ -19,6 +22,21 @@ const HamburguerMENU = () => {
 
 	const DrawerList = (
 		<Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+			<Box
+				sx={{
+					padding: "1rem",
+					margin: "1rem 0 2rem 0",
+					display: "flex",
+					justifyContent: "flex-start",
+					color: "var(--secondary-text-grey)",
+					width: "14px",
+					height: "14px",
+				}}
+			>
+				<IconButton onClick={toggleDrawer(false)} aria-label="close drawer">
+					<CloseIcon />
+				</IconButton>
+			</Box>
 			<List>
 				{["Collections", "Men", "Women", "About", "Contact"].map(text => (
 					<ListItem key={text} disablePadding>
@@ -36,7 +54,10 @@ const HamburguerMENU = () => {
 
 	return (
 		<div>
-			<Button onClick={toggleDrawer(true)}>
+			<Button
+				onClick={toggleDrawer(true)}
+				sx={{ padding: "0px", minWidth: "auto" }}
+			>
 				<span className={styles.hamburguerMENU} />
 			</Button>
 			<Drawer open={open} onClose={toggleDrawer(false)}>
