fdca02d40e0db098affd4534d776603f140bf877
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-17 23:08:25 -0300

    Adding some comments to the hooks for better understanding them

--- src/hooks/useAutoLoop.ts
+++ src/hooks/useAutoLoop.ts
@@ -1,17 +1,28 @@
+// Custom hook to handle automatic looping of a function at a specified interval.
+
 import { useEffect, useRef } from "react";
+
+/**
+ * Custom hook to handle automatic looping of a function at a specified interval.
+ *
+ * @param callback - The function to be called at each interval.
+ * @param intervalTime - The time in milliseconds between each call.
+ */
 
 const useAutoLoop = (callback: () => void, interval: number) => {
 	const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
 	useEffect(() => {
+		// Set up the interval to call the provided callback function repeatedly.
 		intervalRef.current = setInterval(callback, interval);
 
+		// Clean up the interval when the component unmounts or interval changes.
 		return () => {
 			if (intervalRef.current) {
 				clearInterval(intervalRef.current);
 			}
 		};
-	}, [callback, interval]);
+	}, [callback, interval]); // Dependencies array: re-run effect if callback or intervalTime changes
 };
 
 export default useAutoLoop;
--- src/hooks/useSwiperControls.ts
+++ src/hooks/useSwiperControls.ts
@@ -1,15 +1,31 @@
+// Custom hook to handle the functionality of previous and next buttons in a swiper.
+
 import { useCallback } from "react";
 
+/**
+ * @param setCurrentImageIndex - State setter function to update the current image index.
+ * @param imageCount - Total number of images in the swiper.
+ *
+ * @returns An object containing functions to handle previous and next button clicks.
+ */
 const useSwiperControls = (
 	setCurrentImageIndex: React.Dispatch<React.SetStateAction<number>>,
 	productImagesLength: number
 ) => {
+	/**
+	 * Handle click event for the previous button.
+	 * Moves to the previous image, or loops back to the last image if on the first one.
+	 */
 	const handlePreviousClick = useCallback(() => {
 		setCurrentImageIndex(prevIndex =>
 			prevIndex === 0 ? productImagesLength - 1 : prevIndex - 1
 		);
 	}, [setCurrentImageIndex, productImagesLength]);
 
+	/**
+	 * Handle click event for the next button.
+	 * Moves to the next image, or loops back to the first image if on the last one.
+	 */
 	const handleNextClick = useCallback(() => {
 		setCurrentImageIndex(prevIndex =>
 			prevIndex === productImagesLength - 1 ? 0 : prevIndex + 1
