c5e8d50978dfb1a44b47cc2fd1b1c75b6be6c2d0
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-17 23:49:53 -0300

    Succesfully implementing MUI Drawer into the project

--- src/components/NavBar/HamburguerMENU/index.tsx
+++ src/components/NavBar/HamburguerMENU/index.tsx
@@ -1,7 +1,46 @@
+// CSS
 import styles from "./HamburguerMENU.module.css";
+// MUI Drawer imports
+import React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 
 const HamburguerMENU = () => {
-	return <button className={styles.hamburguerMENU} />;
+	const [open, setOpen] = React.useState(false);
+
+	const toggleDrawer = (newOpen: boolean) => () => {
+		setOpen(newOpen);
+	};
+
+	const DrawerList = (
+		<Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+			<List>
+				{["Collections", "Men", "Women", "About", "Contact"].map(text => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton>
+							<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
+		</Box>
+	);
+
+	return (
+		<div>
+			<Button onClick={toggleDrawer(true)}>
+				<span className={styles.hamburguerMENU} />
+			</Button>
+			<Drawer open={open} onClose={toggleDrawer(false)}>
+				{DrawerList}
+			</Drawer>
+		</div>
+	);
 };
 
 export default HamburguerMENU;
