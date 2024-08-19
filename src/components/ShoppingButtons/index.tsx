de59797e6d4ab60aae979428a8c510074297cf8f
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-19 19:45:08 -0300

    Adding the item count manipulation to the interface

--- src/components/ShoppingButtons/index.tsx
+++ src/components/ShoppingButtons/index.tsx
@@ -4,17 +4,32 @@ import ShoppingButton from "../ShoppingButton";
 import IconShoppingCart from "../icons/IconShoppingCart";
 import IconShoppingMinus from "../icons/IconShoppingMinus";
 import IconShoppingPlus from "../icons/IconShoppingPlus";
+import { useState } from "react";
 
 function ShoppingButtons() {
+	const [itemCount, setItemCount] = useState(0);
+
+	const handleIncrement = () => {
+		setItemCount(prevCount => prevCount + 1);
+	};
+
+	const handleDecrement = () => {
+		setItemCount(prevCount => (prevCount > 0 ? prevCount - 1 : 0));
+	};
+
 	return (
 		<div className={styles.shoppingButtons}>
 			<ShoppingButton
 				$backgroundColor="var(--light-greyish-blue)"
 				$justifyContent="space-between"
 			>
-				<IconShoppingMinus />
-				0
-				<IconShoppingPlus />
+				<button onClick={handleDecrement}>
+					<IconShoppingMinus />
+				</button>
+				{itemCount}
+				<button onClick={handleIncrement}>
+					<IconShoppingPlus />
+				</button>
 			</ShoppingButton>
 			<ShoppingButton
 				$backgroundColor="var(--primary-orange)"
