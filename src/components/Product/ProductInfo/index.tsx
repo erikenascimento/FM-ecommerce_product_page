69a867ecc50b851d21a3882fbcd1ac1f24f0f626
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-21 20:51:55 -0300

    Refactoring the static data for the data from the product json file

--- src/components/Product/ProductInfo/index.tsx
+++ src/components/Product/ProductInfo/index.tsx
@@ -1,27 +1,34 @@
+// Custom components
 import ShoppingButtons from "@/components/ShoppingButtons";
-
+// Css
 import styles from "./ProductInfo.module.scss";
+// Data
+import productData from "@/data/productData.json";
 
 function ProductInfo() {
 	return (
 		<>
 			<article className={styles.productArticle}>
 				<span className={styles["productArticle__brandName"]}>
-					Sneaker Company
+					{productData.product.company}
 				</span>
 				<h1 className={styles["productArticle__title"]}>
-					Fall Limited Edition Sneakers
+					{productData.product.title}
 				</h1>
 				<p className={styles["productArticle__description"]}>
-					These low-profile sneakers are your perfect casual wear companion.
-					Featuring a durable rubber outer sole, they`&apos`ll withstand
-					everything the weather can offer.
+					{productData.product.description}
 				</p>
 			</article>
 			<div className={styles["pricing"]}>
-				<span className={styles["pricing__finalPrice"]}>$125.00</span>
-				<span className={styles["pricing__discount"]}>50%</span>
-				<span className={styles["pricing__fullPrice"]}>$250.00</span>
+				<span className={styles["pricing__finalPrice"]}>
+					${productData.product.pricing.discountedPrice.toFixed(2)}
+				</span>
+				<span className={styles["pricing__discount"]}>
+					{productData.product.pricing.discountRate * 100}%
+				</span>
+				<span className={styles["pricing__fullPrice"]}>
+					{productData.product.pricing.fullPrice.toFixed(2)}
+				</span>
 			</div>
 			<ShoppingButtons />
 		</>
