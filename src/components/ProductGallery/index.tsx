ecd2df7af5ad4f9924709701f7990e2c4980b312
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-21 20:24:47 -0300

    Staticly serving the image to their proper elements

--- src/components/ProductGallery/index.tsx
+++ src/components/ProductGallery/index.tsx
@@ -1,7 +1,28 @@
+// CSS
 import styles from "./ProductGallery.module.scss";
+// Data
+import productData from "@/data/productData.json";
 
 const ProductGallery = () => {
-	return <div className={styles.productGallery}>ProductGallery</div>;
+	const productImages = productData.product.images;
+	return (
+		<section className={styles.productGallery}>
+			<img
+				className={"[styles.productGallery__highlighted]"}
+				src={productImages.productImages[1]}
+				alt="Product image number 1"
+			/>
+			<ul className={"[styles.productGallery__thumbnails]"}>
+				{productImages.productThumbnails.map((imageSrc, index) => (
+					<img
+						key={index}
+						src={imageSrc}
+						alt={`Product thumbnail number ${index + 1}`}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default ProductGallery;
