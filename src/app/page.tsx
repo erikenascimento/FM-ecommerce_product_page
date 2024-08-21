5720d210364cc35f3e3773a524b94d6bd2fa5b78
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-20 21:44:46 -0300

    Creating the divider to be displayed in larger layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import NavBar from "@/components/NavBar";
 import ProductInfo from "@/components/Product/ProductInfo";
 import ProductSwiper from "@/components/Product/ProductSwiper";
+import SectionDivider from "@/components/SectionDivider";
 // For the cart component
 import { CartProvider } from "@/context/CartProvider";
 
@@ -11,6 +12,7 @@ export default function Home() {
 		<>
 			<CartProvider>
 				<NavBar />
+				<SectionDivider />
 				<main>
 					<ProductSwiper />
 					<ProductInfo />
---
+++ src/components/SectionDivider/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./SectionDivider.module.scss";
+
+const SectionDivider = () => {
+	return <hr className={styles.sectionDivider} />;
+};
+
+export default SectionDivider;
