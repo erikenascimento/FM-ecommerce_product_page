1269063d407da3ad97cd8efc7b72e03bdb8fb929
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-21 20:38:25 -0300

    Applying some more styling

--- src/components/ProductGallery/index.tsx
+++ src/components/ProductGallery/index.tsx
@@ -8,17 +8,18 @@ const ProductGallery = () => {
 	return (
 		<section className={styles.productGallery}>
 			<img
-				className={"[styles.productGallery__highlighted]"}
+				className={styles.productGallery__highlighted}
 				src={productImages.productImages[1]}
 				alt="Product image number 1"
 			/>
-			<ul className={"[styles.productGallery__thumbnails]"}>
+			<ul className={styles.productGallery__thumbnails}>
 				{productImages.productThumbnails.map((imageSrc, index) => (
-					<img
+					<li
+						className={styles.productGallery__thumbnails__listItem}
 						key={index}
-						src={imageSrc}
-						alt={`Product thumbnail number ${index + 1}`}
-					/>
+					>
+						<img src={imageSrc} alt={`Product thumbnail number ${index + 1}`} />
+					</li>
 				))}
 			</ul>
 		</section>
