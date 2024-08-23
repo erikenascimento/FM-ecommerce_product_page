a22d77cc905f2286e583366ee5d558f6cedc47a2
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-23 18:29:15 -0300

    This will apply the outline to all thumbnails on click

--- src/components/ProductGallery/index.tsx
+++ src/components/ProductGallery/index.tsx
@@ -1,10 +1,21 @@
 // CSS
+import { useState } from "react";
+// React Hooks
 import styles from "./ProductGallery.module.scss";
 // Data
 import productData from "@/data/productData.json";
 
 const ProductGallery = () => {
+	// Creating a cosnt to shorten the pointer name to the image
 	const productImages = productData.product.images;
+
+	// To add the outline effect when selected
+	const [isActive, setIsActive] = useState(false);
+
+	const toggleClass = () => {
+		setIsActive(!isActive);
+	};
+
 	return (
 		<section className={styles.productGallery}>
 			<img
@@ -17,8 +28,13 @@ const ProductGallery = () => {
 					<li
 						className={styles.productGallery__thumbnails__listItem}
 						key={index}
+						onClick={toggleClass}
 					>
-						<img src={imageSrc} alt={`Product thumbnail number ${index + 1}`} />
+						<img
+							src={imageSrc}
+							alt={`Product thumbnail number ${index + 1}`}
+							className={isActive ? styles.imageOutlined : ""}
+						/>
 					</li>
 				))}
 			</ul>
